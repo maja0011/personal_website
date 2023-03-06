f57fd32696541055c58b5d0449b9807559c4f38d
maja0011 <maja0011@algonquinlive.com>
2023-03-06 02:37:40 -0500

    Nav Bar completed

---
+++ src/components/about.js
@@ -0,0 +1,17 @@
+import '../css/about.css';
+
+function About () {
+    return (
+        <section id='about'>
+            <h5>Get To Know</h5>
+            <h2>About Me</h2>
+
+            <div className="container about__conatiner">
+                .about__
+            </div>
+        </section>
+
+    );
+}
+
+export default About;
