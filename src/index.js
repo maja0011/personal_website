7b8a8e24c04068773aecd0cbf7c1d15917fb4c6c
maja0011 <maja0011@algonquinlive.com>
2023-04-22 02:46:11 -0400

    made changes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ReactDOM } from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App/>
-  
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
