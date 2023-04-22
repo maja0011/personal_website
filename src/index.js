49aba0bb5deab338754df2a5480d62b475b9e86a
maja0011 <maja0011@algonquinlive.com>
2023-04-22 02:29:05 -0400

    Removed router

--- src/index.js
+++ src/index.js
@@ -2,24 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <div>
-      <Routes>
-        <Route path="/" element ={<App/>}/>
-          {/* <Route exact path="/about" component={About} activeClassName="active"/>
-          <Route exact path="/projects" component={Projects} activeClassName="active"/> */}
-      </Routes>
-        </div>
-    
-    </Router>
-   
-  </React.StrictMode>
+  <App/>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
